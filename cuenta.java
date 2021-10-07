5983053e8cced5c9ccff0e193216e43c98b0e781
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-10-07 23:28:28 +0200

    cuenta.java - errores arreglados 🔧

--- cuenta.java
+++ cuenta.java
@@ -13,50 +13,59 @@ public class cuenta {
         this.saldo = saldo;
     }
 
+    // Metodo para mostrar los datos de la cuenta
     public String ver() {
         return "El nombre de la cuenta es " + nombre + " el numero es " + numero + " el interes es " + interes
-                + " el saldo es " + saldo + "\n";
+                + " el saldo es " + saldo + " € \n";
     }
 
+    // Metodo para ingresar dinero a la cuenta
     public void ingreso(double cantidad) {
         if (cantidad > 0) {
-            saldo += cantidad;
+            saldo += cantidad; // suma la cantidad a la cuenta
+            System.out.println("Se han ingresado " + cantidad + " € a la cuenta");
         } else {
             System.out.println("No se puede ingresar una cantidad negativa");
         }
     }
 
+    // Metodo para retirar dinero de la cuenta
     public void reintegro(double cantidad) {
-        saldo -= cantidad;
+        saldo -= cantidad; // resta la cantidad a la cuenta
     }
 
-    public void transferir(String origen, String destino, double cantidad) {
+    // Metodo para transferir dinero de una cuenta a otra
+    public void transferir(cuenta origen, cuenta destino, double cantidad) {
         if (cantidad > 0) {
-            if (origen.equals(numero)) {
-                saldo -= cantidad;
-                System.out.println("Transferencia realizada");
-            }
+            System.out.println("Transfiriendo " + cantidad + "€ de " + origen.numero + " a " + destino.numero);
+            saldo -= cantidad;
+            destino.saldo += cantidad;
+            System.out.println("Transferencia realizada");
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Introduce el nombre de la cuenta: ");
         String nombre = sc.nextLine();
         System.out.print("Introduce el numero de la cuenta: ");
         String numero = sc.nextLine();
         System.out.print("Introduce el interes de la cuenta: ");
-        double interes = sc.nextDouble();
+        double interes = sc.nextDouble(); // El interés debe ser de tipo float porque no es un numero entero y no double
         System.out.print("Introduce el saldo de la cuenta: ");
         double saldo = sc.nextDouble();
         cuenta cuenta1 = new cuenta(nombre, numero, interes, saldo);
-        cuenta cuenta2 = new cuenta("Juan Ferrández Rubio", "12345678901234567890", 1.75, 300);
         System.out.print(cuenta1.ver());
-        System.out.print("Introduce la cantidad a ingresar en cuenta1: ");
+        cuenta cuenta2 = new cuenta("Juan Ferrández Rubio", "12345678901234567890", 1.75, 300);
+        cuenta cuenta3 = cuenta2;
+        System.out.print("Introduce la cantidad a ingresar en " + "#" + cuenta1.numero + ": ");
         double cantidad = sc.nextDouble();
         cuenta1.ingreso(cantidad);
-        System.out.println(cuenta1.ver());
-        System.out.print(cuenta2.ver());
+        cuenta1.ver();
+        cuenta2.ver();
+        cuenta3.ver();
+        cuenta3.transferir(cuenta3, cuenta2, 10.0);
 
         sc.close();
     }
