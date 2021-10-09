83707d9aabf7f5b194df4cd1dc4dc5a887159c30
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-10-09 22:07:25 +0200

    cuenta.java - colores añadios 💻

--- cuenta/src/cuenta.java
+++ cuenta/src/cuenta.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
 public class cuenta {
+    // Colores para el texto
+    public static final String red = "\u001B[31m";
+    public static final String green = "\u001B[32m";
+    public static final String yellow = "\u001B[33m";
+    public static final String blue = "\u001B[34m";
+    public static final String reset = "\u001B[0m"; // volver al color inicial
+
     private String nombre;
     private String numero;
     private double interes;
     private double saldo;
 
     public cuenta() {
-    } // constructor vacio
+    }
 
     // Setters de la clase cuenta
     public void setNombre(String nombre) {
@@ -28,17 +35,18 @@ public class cuenta {
 
     // Metodo para mostrar los datos de la cuenta
     public String ver() {
-        return "El nombre de la cuenta es " + nombre + " el numero es " + numero + " el interes es " + interes
-                + " el saldo es " + saldo + " € \n";
+        return green + "[ i ] El nombre de la cuenta es " + nombre + " el numero es " + numero + " el interes es "
+                + interes + " el saldo es " + saldo + " € \n" + reset;
     }
 
     // Metodo para ingresar dinero a la cuenta
     public void ingreso(double cantidad) {
         if (cantidad > 0) {
             saldo += cantidad; // suma la cantidad a la cuenta
-            System.out.println("Se han ingresado " + cantidad + " € a la cuenta");
+            System.out.println(
+                    green + "[ i ] Se han ingresado " + cantidad + " € a la cuenta " + "#" + this.numero + reset);
         } else {
-            System.out.println("No se puede ingresar una cantidad negativa");
+            System.out.println(red + "[ x ] No se puede ingresar una cantidad negativa" + reset);
         }
     }
 
@@ -50,24 +58,24 @@ public class cuenta {
     // Metodo para transferir dinero de una cuenta a otra
     public void transferir(cuenta origen, cuenta destino, double cantidad) {
         if (cantidad > 0) {
-            System.out.println(
-                    "Transfiriendo " + cantidad + "€ de " + "#" + origen.numero + " a " + "#" + destino.numero + "...");
+            System.out.println(green + "[ i ] Transfiriendo " + cantidad + "€ de " + "#" + origen.numero + " a " + "#"
+                    + destino.numero + "..." + reset);
             saldo -= cantidad;
             destino.saldo += cantidad;
-            System.out.println("Transferencia realizada");
+            System.out.println(green + "[ i ] Transferencia realizada" + reset);
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Introduce el nombre de la cuenta: ");
+        System.out.print(blue + "[ ? ] Introduce el nombre de la cuenta: " + reset);
         String nombre = sc.nextLine();
-        System.out.print("Introduce el numero de la cuenta: ");
+        System.out.print(blue + "[ ? ] Introduce el numero de la cuenta: " + reset);
         String numero = sc.nextLine();
-        System.out.print("Introduce el interes de la cuenta: ");
+        System.out.print(blue + "[ ? ] Introduce el interes de la cuenta: " + reset);
         double interes = sc.nextDouble();
-        System.out.print("Introduce el saldo de la cuenta: ");
+        System.out.print(blue + "[ ? ] Introduce el saldo de la cuenta: " + reset);
         double saldo = sc.nextDouble();
         cuenta cuenta1 = new cuenta();
 
@@ -85,7 +93,7 @@ public class cuenta {
 
         cuenta cuenta3 = cuenta2;
 
-        System.out.print("Introduce la cantidad a ingresar en " + "#" + cuenta1.numero + ": ");
+        System.out.print(blue + "[ ? ] Introduce la cantidad a ingresar en " + "#" + cuenta1.numero + ": " + reset);
         double cantidad = sc.nextDouble();
         cuenta1.ingreso(cantidad);
         cuenta1.ver();
